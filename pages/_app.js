f3b28afe0a3b5cb9533f67ce4a317abf368f6d5d
quabynah-bilson <84394923+quabynah-bilson@users.noreply.github.com>
2021-09-17 09:11:07 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import "../styles/globals.css";
 import "../utils/firebase";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    return null;
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
